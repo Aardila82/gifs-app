f53af1e346353180dd51acb392ea4fda016d8ed1
Alexander <desarrollador1@arsuarez.com>
2025-03-10 10:48:13 -0500

    se separa por componentes el menu y el nav

---
+++ src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'gifs-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.component.html',
+  styleUrl: './side-menu-header.component.css'
+})
+export class SideMenuHeaderComponent {
+
+}
