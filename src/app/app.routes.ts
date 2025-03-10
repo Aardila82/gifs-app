77c735edb6113b890defaa2a3d25aa212bd7329a
Alexander <desarrollador1@arsuarez.com>
2025-03-09 19:13:51 -0500

    Primer commit - Proyecto Angular

---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./gifs/pages/dashboard-page/dashboard-page.component')
+  },
+
+  {
+    path:'**',
+    redirectTo: 'dashboard'
+  }
+
+
+];
